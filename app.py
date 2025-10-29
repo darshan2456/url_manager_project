83990c85aad53ac5229c1d58ba884a601473ad9c
noobcoder <darshanmparekh245624@gmail.com>
2025-10-30 01:44:13 +0530

    change in app.py from remote repo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "the changes are seen damnn! "
+    return "the changes are seen damnn! this is a change from remote repo "
 
 if __name__ == '__main__':
     app.run(debug=True)
