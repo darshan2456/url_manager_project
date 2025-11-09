f6aff19f0af5c54bafcba791930a899a4f2468aa
darshan2456 <darshanmparekh245624@gmail.com>
2025-11-09 20:20:51 +0530

    added hardcoded tablename for Tag class in python as 'tag'

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ class URL(db.Model):
     is_archived = db.Column(db.Boolean, default=False)
 
 class Tag(db.Model):
+    __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     color = db.Column(db.String(7))
